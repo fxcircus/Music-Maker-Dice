57861631c10c3ac12d6d108bbbc2e71979c8eb4f
Roy Daniel <roydanielr@gmail.com>
2022-01-28 20:09:34 -0500

    comments and font size

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
+// Global Vars and arrays:
 const maxBpm = 140
 const minBpm = 75
 let scaleIdx = 0
-
 const roots = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#' , 'G', 'G#']
-const scales = ['MAJOR', 'MINOR', 'DORIAN', 'PHRYGIAN', 'LYDIAN', 'MIXOLYDIAN', 'LOCRIAN']
+const scales = ['MAJOR', 'MINOR', 'DORIAN', 'PHRYGI', 'LYDIAN', 'MIXO', 'LOCRI']
 const sounds = ['GUITAR', 'BASS', 'PERC', 'PAD', 'SYNTH', 'ARP', 'ACC', 'BOW', 'COIN', 'FX', 'LEAD', 'FUZZ', 'HARMONIX','EBOW','FREEZE', 'LAP STEEL']
 const scaleTones = ['T - T - S - T - T - T - S', 'T - S - T - T - S - T - T', 'T - S - T - T - T - S - T', 'S - T - T - T - S - T - T', 'T - T - S - T - S - T - T', 'T - T - S - T - T - S - T', 'S - T - T - S - T - T - T']
 
+// DOM elements:
 const cubeEls = document.querySelectorAll('cube')
 const rootEl = document.getElementById('root-text')
 const scaleEl = document.getElementById('scale-text')
@@ -15,14 +16,19 @@ const soundEl = document.getElementById('sound-text')
 const btnEl = document.getElementById('new-button')
 const tonesEl = document.getElementById('scale-tones')
 
+btnEl.addEventListener('click', (evt) => {
+    rollDice()
+  })
+
+// Random Index Generator Function:
 const getIndex = (num) => {
   let result = Math.floor(Math.random() * num)
   scaleIdx = result
   return result
 }
 
+// New Roll Function:
 const rollDice = () => {
-
   rootEl.textContent = roots[getIndex(roots.length)]
 
   scaleEl.textContent = scales[getIndex(scales.length)]
@@ -43,8 +49,5 @@ const rollDice = () => {
   bpmEl.textContent = bpmVal
 }
 
-btnEl.addEventListener('click', (evt) => {
-  rollDice()
-})
-
+// Initial choices:
 rollDice()
